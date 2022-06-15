e173c5e350dafd13f78fc37ff13abbecaac210cf
T.L. de Mast <teundemast@gmail.com>
2022-06-15 09:05:50 +0200

    Update generate_dataset.py

--- src/scripts/generate_dataset.py
+++ src/scripts/generate_dataset.py
@@ -330,8 +330,8 @@ strfformat_ensurance = "%d/%m/%Y"
 
 
 dir_ = '/local/s2656566/wateroverlast/regenwater_overlast/src/data/'
-input_file = f"postcode6/postcode6_number1.json"
-output_file = f'pkls/postcode6/postcode6_number1.pkl'
+input_file = f"postcode6/postcode6_number2.json"
+output_file = f'pkls/postcode6/postcode6_number2.pkl'
 
 # Time variables
 total = 0
