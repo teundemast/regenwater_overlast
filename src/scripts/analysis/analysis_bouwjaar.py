410212d0aec4777a8fabfa4c51c925c6e2c49411
T.L. de Mast <teundemast@gmail.com>
2022-07-30 17:21:50 +0200

    Update analysis_bouwjaar.py

--- src/scripts/analysis/analysis_bouwjaar.py
+++ src/scripts/analysis/analysis_bouwjaar.py
@@ -6,8 +6,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
 from sklearn import metrics
-from sklearn.tree import export_graphviz
-from subprocess import call
+from matplotlib import pyplot as plt
+
 
 resultFolder ="/local/s2656566/wateroverlast/regenwater_overlast/results/result_texts/"
 resultFile = open (resultFolder +"result_precise_bouwjaar_just.txt", "w+") 
@@ -40,75 +40,95 @@ accuracyResult = []
 precisionResult = []
 recallResult = []
 totalConfusion = [[0,0],[0,0]]
-for i in range(10):
-    listofarr = []
-    path = "precise_bouwjaar.pkl"
-    df = pd.read_pickle(f"/local/s2656566/wateroverlast/regenwater_overlast/src/data/pkls/{path}").reset_index()
-    df = df.dropna()
-    df = df[["target", "bouwjaar", "date"]]
-#     df = df[["target", "layers", "bouwjaar", "past3hours", "date"]]
-#     df[column] = df.apply(normalize, axis=1)
-#     df[column] = df.apply(reshape, axis=1)
 
-#     concat_df = pd.concat(listofarr)
+listofarr = []
+path = "precise_bouwjaar.pkl"
+df = pd.read_pickle(f"/local/s2656566/wateroverlast/regenwater_overlast/src/data/pkls/{path}").reset_index()
+df = df.dropna()
+# df = df[["target", "bouwjaar", "date"]]
+df = df[["target", "layers", "bouwjaar", "past3hours", "date"]]
+df[column] = df.apply(normalize, axis=1)
+df[column] = df.apply(reshape, axis=1)
 
-#     df = concat_df.dropna(axis="columns", how="all")
-#     df = df.reset_index(drop=True)
-#     df= df.drop(columns=['indexl', 'index'])
+concat_df = pd.concat(listofarr)
 
-    a_tenth = int(len(df.index) / 10)
-    test_frame = df.sample(a_tenth)
-    # print(test_frame)
-    dates_test_frame = test_frame["date"].tolist()
-    # print(len(df.index))
-    training_frame = df[~df["date"].isin(dates_test_frame)]
-    # print(len(training_frame.index))
-    training_frame = training_frame.sample(1700)
-    # print(training_frame)
+df = concat_df.dropna(axis="columns", how="all")
+df = df.reset_index(drop=True)
+df= df.drop(columns=['indexl', 'index'])
+
+
     
-    training_frame = training_frame.drop(columns=["date"])
-    test_frame = test_frame.drop(columns=["date"])
+labels = np.array(df['target'])
 
-    training_labels = np.asarray(training_frame['target'])
-    training_labels = training_labels.astype('int')
-    list_training_features = list(training_frame.drop(columns=['target']).columns)
-    training_features = np.asarray(training_frame.drop(columns=['target']))
-    training_features = training_features.astype('float')
-    test_labels = np.asarray(test_frame['target'])
-    test_labels = test_labels.astype('int')
-    test_features = np.asarray(test_frame.drop(columns=['target']))
-    test_features = test_features.astype('float')
+#set features and convert to numpy array
+#with height: features= rainTweets_eq.drop(columns=['radarX', 'radarY', 'date', 'text','tiffile', 'height','labels'])
+#features= rainTweets_eq.drop(columns=['labels'])
+features= df.drop(columns=['target'])
+#features = rainTweets_eq[['rain']]
 
-    rf = RandomForestClassifier(n_estimators = 1000, random_state = i)
-    # print(training_labels)
-    print(training_features)
-    rf.fit(training_features, training_labels)
-    # estimator = rf.estimators_[5]
-    # export_graphviz(estimator, out_file='tree.dot', 
-    #             feature_names = list_training_features,
-    #             class_names = ['0', '1'],
-    #             rounded = True, proportion = False, 
-    #             precision = 2, filled = True)
-    # call(['dot', '-Tpng', 'tree.dot', '-o', f'tree{i}.png', '-Gdpi=600'])
-    print(test_features)
-    label_prediction = rf.predict(test_features)  
-    confusion = confusion_matrix(test_labels,label_prediction)
-    totalConfusion[0][0] += confusion[0][0]
-    totalConfusion[0][1] += confusion[0][1]
-    totalConfusion[1][0] += confusion[1][0]
-    totalConfusion[1][1] += confusion[1][1]
+# Saving feature names for later use
+feature_list = list(features.columns)
 
-    accuracyResult.append(metrics.accuracy_score(test_labels, label_prediction))
-    precisionResult.append(precision_score(test_labels, label_prediction))
-    recallResult.append(recall_score(test_labels, label_prediction))
+features = np.array(features)
 
-    resultFile.write("Fold "+str(i)+"\n")
-    resultFile.write(str(confusion)+'\n')
-    resultFile.write("Accuracy: "+str(metrics.accuracy_score(test_labels, label_prediction))+"\n")
-    resultFile.write("Precision: "+str(precision_score(test_labels, label_prediction))+"\n")
-    resultFile.write("Recall: "+str(recall_score(test_labels, label_prediction)) + "\n\n")
-resultFile.write("\nAverage accuracy: "+str(np.average(accuracyResult))+"\n")
-resultFile.write("Average precision: "+str(np.average(precisionResult))+"\n")
-resultFile.write("Average recall: "+ str(np.average(recallResult))+"\n")
-resultFile.write("Total Confusion matrix: \n["+str(totalConfusion[0][0])+","+ str(totalConfusion[0][1])+"] \n"+"["+str(totalConfusion[1][0])+","+ str(totalConfusion[1][1])+"] \n")
-resultFile.close()
+#k-fold cross validation
+skf = StratifiedKFold(n_splits=10)
+mape = []
+treeNumber = 0
+accuracyResult = []
+precisionResult = []
+recallResult = []
+totalConfusion = [[0,0],[0,0]]
+for train_index, test_index in skf.split(features, labels):
+        #print("Train: ", train_index, " Test: ", test_index)
+        train_features, test_features = features[train_index], features[test_index]
+        train_labels, test_labels = labels[train_index], labels[test_index]
+
+        #print(test_features[0])
+        #train and test the decision tree
+        rf = RandomForestClassifier(n_estimators = 1000, random_state = 42)        
+        rf.fit(train_features, train_labels)
+        label_prediction = rf.predict(test_features)
+        confusion = confusion_matrix(test_labels,label_prediction)
+        totalConfusion[0][0] += confusion[0][0]
+        totalConfusion[0][1] += confusion[0][1]
+        totalConfusion[1][0] += confusion[1][0]
+        totalConfusion[1][1] += confusion[1][1]
+
+        accuracyResult.append(metrics.accuracy_score(test_labels, label_prediction))
+        precisionResult.append(precision_score(test_labels, label_prediction))
+        recallResult.append(recall_score(test_labels, label_prediction))
+
+        resultFile.write("Fold "+str(treeNumber)+"\n")
+        resultFile.write(str(confusion)+'\n')
+        resultFile.write("Accuracy: "+str(metrics.accuracy_score(test_labels, label_prediction))+"\n")
+        resultFile.write("Precision: "+str(precision_score(test_labels, label_prediction))+"\n")
+        resultFile.write("Recall: "+str(recall_score(test_labels, label_prediction)) + "\n\n")
+
+        # tree = rf.estimators_[4]# Import tools needed for visualization
+        # from sklearn.tree import export_graphviz
+        # import pydot# Pull out one tree from the forest
+        # tree = rf.estimators_[5]# Export the image to a dot file
+        # outputFile = resultFolder+"tree"+str(treeNumber)+".dot"
+        # export_graphviz(tree, out_file = outputFile, feature_names = feature_list, rounded = True, precision = 1)# Use dot file to create a graph
+        # #(graph, ) = pydot.graph_from_dot_file('tree.dot')# Write graph to a png file
+        # #graph.write_png('tree.png')
+        treeNumber+=1
+
+        #output cross validation performance
+        #all_accuracies = cross_val_score(estimator=rf, X=features, y=labels, cv=10)
+
+        fig, ax = plt.subplots()
+        data = [accuracyResult, precisionResult, recallResult]
+        xlabels = ["Accuracy", "Precision", "Recall"]
+        ax.boxplot(data)
+        ax.set_xticklabels(xlabels)
+        ax.set_ylim(0,1)
+
+        resultFile.write("\nAverage Accuracy: "+str(np.average(accuracyResult))+"\n")
+        resultFile.write("Average Precision: "+str(np.average(precisionResult))+"\n")
+        resultFile.write("Average Recall: "+ str(np.average(recallResult))+"\n")
+        resultFile.write("Total Confusion matrix: \n["+str(totalConfusion[0][0])+","+ str(totalConfusion[0][1])+"] \n"+"["+str(totalConfusion[1][0])+","+ str(totalConfusion[1][1])+"] \n")
+        resultFile.close()
+        #print(cross_val_score(estimator=rf, X=features, y=labels, cv=skf, scoring="accuracy"))
+        plt.savefig(resultFolder+"boxplotMeasures.png")
