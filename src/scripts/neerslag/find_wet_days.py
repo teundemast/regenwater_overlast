d0222922c15903d1b8d51b935e023e19c2f15a33
T.L. de Mast <teundemast@gmail.com>
2022-08-04 11:37:08 +0200

    Update find_wet_days.py

--- src/scripts/neerslag/find_wet_days.py
+++ src/scripts/neerslag/find_wet_days.py
@@ -3,14 +3,21 @@ import precipitation_nl
 
 PNL = precipitation_nl.PrecipitationNL(queue_size=300)
 
-def find_wet_days(year):
+def find_wet_days(year, out_file):
     wet_days = []
     for month in range(1,13):
         for day in range(1,28):
             day_fall = PNL.get_precipation_data_past_hours_list(year, month, day, 23, 59, 52.1092717, 5.1809676, 23)
             sum_day_fall = sum(day_fall)
-            print(sum_day_fall)
+            if sum_day_fall > 700:
+                wet_days.append(f"{year}-{month}-{day}")
+    with open(out_file, 'w') as f:   
+        for day in wet_days:
+            f.write(day)
+        
             
 if __name__ == '__main__':
-    find_wet_days(2017)
+    year = 2017
+    out_file = "wet_days{year}.txt"
+    find_wet_days(2017, out_file)
             
