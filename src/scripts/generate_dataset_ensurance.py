39f1c9cf2199bdb7fac03efb87c37f974a2129fd
Teun De Mast <teundemast@gmail.com>
2022-08-11 12:45:24 +0200

    Update generate_dataset_ensurance.py

--- src/scripts/generate_dataset_ensurance.py
+++ src/scripts/generate_dataset_ensurance.py
@@ -50,6 +50,7 @@ def get_precipitation_data_ensurance(row):
     rain = PNL.get_precipation_data_past_hours_list(date.year, date.month, date.day, 23, 59, lat, lon, 24)
     print(rain)
     time.sleep(1)
+    print(sum(rain))
     peak = 0
     for idx, sum in enumerate(rain):
         try: 
